d5c4b04ec94c2abc127868646aa5347e1f71c0be
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-13 16:45:57 +0100

    [Feat] Adding authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const routes = express.Router();
 
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
 routes.post('/users', UsersController.postNew);
+routes.get('/connect', AuthController.getConnect);
+routes.get('/disconnect', AuthController.getDisconnect);
+routes.get('/users/me', UserController.getMe);
 
 export default routes;
