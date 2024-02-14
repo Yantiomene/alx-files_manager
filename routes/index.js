78a19d4b5e448bea772dba39355038493badace5
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-14 14:57:52 +0100

    [Feat] First file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const routes = express.Router();
 
@@ -11,5 +12,6 @@ routes.post('/users', UsersController.postNew);
 routes.get('/connect', AuthController.getConnect);
 routes.get('/disconnect', AuthController.getDisconnect);
 routes.get('/users/me', UsersController.getMe);
+routes.post('/files', FilesController.postUpload);
 
 export default routes;
