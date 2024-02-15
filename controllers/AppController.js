01641d87de923086c8de1cf16e7408b4868e8acf
esmond-adjei <xmongram@gmail.com>
2024-02-15 02:06:36 +0000

    [task 2] still experimenting

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,13 +5,15 @@ const AppController = {
   getStatus: (req, res) => {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-    res.status(200).json({ redis: redisAlive, db: dbAlive }).end();
-  },
+    res.set('Content-Type', 'application/json');
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+},
 
-  getStats: async (req, res) => {
+getStats: async (req, res) => {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
-    res.status(200).json({ users: usersCount, files: filesCount }).end();
+    res.set('Content-Type', 'application/json');
+    res.status(200).json({ users: usersCount, files: filesCount });
   },
 };
 
