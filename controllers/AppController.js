b9dd121178da5b96558f4a5c1ae6149fe08a8bfa
esmond-adjei <xmongram@gmail.com>
2024-02-15 02:08:33 +0000

    [task 2] still...

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,15 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 const AppController = {
-  getStatus: (req, res) => {
+  getStatus: async (req, res) => {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-    res.set('Content-Type', 'application/json');
     res.status(200).json({ redis: redisAlive, db: dbAlive });
-},
+  },
 
-getStats: async (req, res) => {
+  getStats: async (req, res) => {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
-    res.set('Content-Type', 'application/json');
     res.status(200).json({ users: usersCount, files: filesCount });
   },
 };
